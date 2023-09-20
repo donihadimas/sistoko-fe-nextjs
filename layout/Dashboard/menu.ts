c655cbc2231863d5b7c398d59d4a49ac27829d1a
Doni Hadimas <donihadimasaprilian@gmail.com>
2023-09-20 09:12:14 +0700

    update: fixing UI side Nav

--- layout/Dashboard/menu.ts
+++ layout/Dashboard/menu.ts
@@ -2,11 +2,56 @@ import { getPath } from "@/lib/const/path";
 
 export const MenuItem = [
   {
+    id: "dashboard",
+    label: "Dashboard",
+    icon: "fa:id-card",
+    href: getPath("index"),
+    // parameter: { action: "list" },
+  },
+  {
     id: "notification",
     label: "Notifikasi",
     icon: "fa:id-card",
     href: getPath("notification"),
     // parameter: { action: "list" },
+  },
+  {
+    id: "notification",
+    label: "Notifikasi",
+    icon: "fa:id-card",
+    href: getPath("notification"),
+    // parameter: { action: "list" },
+  },
+  {
+    id: "notification",
+    label: "Notifikasi",
+    icon: "fa:id-card",
+    href: getPath("notification"),
+    // parameter: { action: "list" },
+  },
+  {
+    id: "settings",
+    label: "Settings",
+    icon: "bi:gear-wide",
+    links: [
+      {
+        id: "setting_action",
+        label: "Action",
+        href: getPath("UMKM_actionSetting"),
+        parameter: { action: "list" },
+      },
+      {
+        id: "setting_pages",
+        label: "Pages",
+        href: getPath("UMKM_pagesSetting"),
+        parameter: { action: "list" },
+      },
+      {
+        id: "setting_setup_page_action",
+        label: "Setup Page Action",
+        href: getPath("UMKM_setupPageAction"),
+      },
+    ],
   },
 ];
 
