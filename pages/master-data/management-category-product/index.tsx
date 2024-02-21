a5b526bd14c1a91bd9877ffc0833dea533563e7f
Doni Hadimas <70116833+donihadimas@users.noreply.github.com>
2024-02-21 07:52:59 +0700

    Development (#8)
    * update: add list menu sidenav penjualan, pembelian, laporan, master-data
    
    * update: add menu settings in dropdown account
    
    * update: add page index for menu
    
    * update: change word ih category product
    
    * update : change fav icon & sidenav icon
    
    * update: change primary color
    
    * update: add realtime datetime
    
    * update : change primary color
    
    * update: change icon
    
    * update: change fontsize and font weight
    
    * update add lib sharp
    
    * Feature/management category product/add list table (#7)
    
    * feat: add new depedency
    
    * feat: add docker configuration
    
    * feat: add list table management category product

--- pages/master-data/management-category-product/index.tsx
+++ pages/master-data/management-category-product/index.tsx
@@ -1,13 +1,13 @@
 import { PageContainer } from "@/components/core/sidebar/PageContainer";
 import { PageContent } from "@/components/core/sidebar/PageContent";
+import TableCategoryProduct from "@/components/modules/management-category-product/TableCategoryProduct";
 import { DashboardLayout } from "@/layout/Dashboard/DashboardLayout";
-import { Stack, Text } from "@mantine/core";
+import { Stack } from "@mantine/core";
 import {
   CustomNextPage,
   GetStaticProps,
   InferGetServerSidePropsType,
 } from "next";
-import { Heading } from "tabler-icons-react";
 
 const ManagementCategoryProduct: CustomNextPage = ({
   encryptedId,
@@ -17,7 +17,7 @@ const ManagementCategoryProduct: CustomNextPage = ({
       <PageContainer title="Manajemen Kategori Produk" fluid>
         <Stack spacing="md">
           <PageContent title="">
-            <Text>Halaman Manajemen Kategori Produk</Text>
+            <TableCategoryProduct />
           </PageContent>
         </Stack>
       </PageContainer>
